05940fdd00f834c68ed8e72746511d69d32dcee7
Syamsul <syamsul@alterra.id>
2019-07-23 23:42:29 +0700

    project views challenge

--- project_views/project_views/project_views/settings.py
+++ project_views/project_views/project_views/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nr%=h0-a7y=n=mq@fp94hc3b^5&ovi_e!c(8kbqqszgdo=s1=+'
+SECRET_KEY = 'n$mj4fc3)ck62#)3c0xu12vmor-ifl)7_9w=vvh%((9_lijx6*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'challenges'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'foto_profil')
